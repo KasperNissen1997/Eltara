8d7749cb6c7341290fc29f5d0124d950cb0443af
Kasper Nissen <KasperNissen1997@gmail.com>
2022-12-17 22:52:09 +0100

    Added the ability to move the player character.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,36 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-    // handle all player movement
+/// <summary>
+/// PlayerController receives and processes user input, and applies movement to an associated Rigidbody2d component. 
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    #region Component references
+    private Rigidbody2D rigidbody2d;
+    #endregion
+
+    [Range(0.1f, 10)]
+    public float speed = 1;
+
+    private float inputHorizontal; 
+    private float inputVertical;
+
+    private Vector2 movementDirection;
+
+    private void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        inputVertical = Input.GetAxisRaw("Vertical");
+
+        movementDirection = new Vector2(inputHorizontal, inputVertical).normalized;
+
+        // attempts to move the rigidbody
+        rigidbody2d.MovePosition(rigidbody2d.position + movementDirection * speed * Time.fixedDeltaTime);
+    }
 }
